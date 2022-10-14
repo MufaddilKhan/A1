d9d7c9af781c6ad3b668f9fce21e0081be761acc
mufaddil <mufaddil2003@gmail.com>
2022-10-14 04:28:52 -0700

    work complete

---
+++ index.dart
@@ -0,0 +1,127 @@
+//  void main() {
+//  var length=5;
+//  var breadth=6;
+//    if(length==breadth){
+//      print("It's square value");
+//    }else{
+//      print("It's rectangle value");
+//    }
+// }
+
+
+// void main() {
+//   int a=20;
+//   int b=60;
+//   int c=30;
+//   if (a>b && a>c)
+//    {
+//        print("The biggest age is A");
+//    }
+//    else
+//    {
+//        print("The lesser age is A");
+//    }
+//    //(b>a) && (b>c)
+//    if (b>a&& b>c)
+//    {
+//        print("The biggest age is B");
+//    }
+//    else
+//    {
+//        print("The lesser age is B");
+//    }
+//    if (c>a && c>a)
+//    {
+//        print("The biggest age is C");
+//    }
+//    else
+//    {
+//        print("sorry");
+//    }
+// }
+// void main() {
+//  int held = 16;
+//   int attended = 10;
+//    var percentage = attended / held *100;
+
+//   if(percentage < 75){
+//          print("You are not allowed to sit in exam");
+
+//   }
+//  else{
+// print("You are allowed to sit in exam");
+
+// }
+// }
+
+// void main() {
+//   int year=2001;
+//  if (year % 400 == 0) {
+//     print(" is a leap year.");
+//   }
+
+//   else if (year % 100 == 0) {
+//     print(" is not a leap year.");
+//   }
+
+//   else if (year % 4 == 0) {
+//     print (" is a leap year.");
+//   }
+  
+//   else {
+//     print(" is not a leap year.");
+//   }
+
+// }
+// void main() {
+// var temp =49;
+//   if (temp<0){
+//   print("Freezing Weather");
+// }else if(temp<=10){
+//       print("Very Cold Weather");
+
+//   }else if(temp<=20){
+//       print(" Cold Weather");
+
+//   }else if(temp<=20-30){
+//       print("normal; Weather");
+
+//   }else if(temp<=30-40){
+//       print("It's hot");
+
+//   }else if(temp>=40){
+//       print("It's very hot");
+
+//   }else{
+//     print("GOOd");
+//   }
+// }void main() {
+//   var two="b"; 
+//   if (two=="a"||two=="e"||two=="i"||two=="o"||two=="u"){
+//         print("Is a vowel.");
+//   }
+//     else{
+//         print("Is a consonant.", );
+//     }
+// }
+
+// void main() {
+// var id =1001;
+//   var name= "James";
+//   var conu =800;
+//   print("Id =$id");
+//   print("Counsumer Name =$name");
+//   print("Unit Consumed =$conu");
+//   if (conu <200 )
+// 	print("Amount Charges =1.20");
+//    else	if (conu>=200 && conu<400)
+// 			print("Amount Charges =1.50");
+
+// 	else if (conu>=400 && conu<600)
+// 			print("Amount Charges=1.80");
+
+// 		else{
+// 			print("Amount Charges =200");
+//     }
+  
+// }
